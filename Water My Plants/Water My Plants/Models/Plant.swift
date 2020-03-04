0b158df319b1ac32ea930b3a6ca651a6e4281ced
Alex Thompson <guitarman226@gmail.com>
2020-03-04 01:16:49 -0600

    Some networking

--- Water My Plants/Water My Plants/Models/Plant.swift
+++ Water My Plants/Water My Plants/Models/Plant.swift
@@ -13,8 +13,8 @@ import UIKit
 struct Plant: Codable, Equatable {
     var nickname: String?
     var species: String?
-//    var id: Int / Made string like model Sal
-    var id: String?
+    var user_id: String?
     var h2oFrequency: String?
     var image: String?
+    var id: String?
 }
